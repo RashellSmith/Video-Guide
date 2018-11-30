37cc64f1909bd3bd13199095bbcd1e6626db6b09
Rashsmit <33126899+Rashsmit@users.noreply.github.com>
2018-11-29 19:19:13 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded",function(){
+let lines = $("subtitles").children() ;
+const video = document.getElementById("video1");
+
+	// Update the progress bar as the video plays
+	video.ontimeupdate =() => {
+		// highlight text as video plays
+		for (let i = 0;i < lines.length; i++) {
+      let Current = video.getCurrentTime();
+      let start =lines[i].getAttribute("data-start");
+      let end= lines[i].getAttribute("data-end");
+		  if (current > start & current < end){
+		    lines[i].style.backgroundcolor = "#e5a01e";
+		  } else {
+		    lines[i].style.backgroundcolor = "";
+      }
+		}
+    }
+  });
+
+
+
+
+// // var video = document.getElementsByTagName("video")
+// var width = window.matchMedia("(max-width:768px)")
+//
+// function myFunction(width){
+//   if (x.matches){
+//     document.getElementsByTagName("video").setAttribute("width", "480");
+//   }
+// }
+//
+//
+// myFunction(width) // Call listener function at run time
+// width.addListener(myFunction) // Attach listener function on state changes
