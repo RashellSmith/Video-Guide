80caab7454b2dc8d6bb3ea755f545a07ba4cbd9b
Rashsmit <33126899+Rashsmit@users.noreply.github.com>
2018-12-06 18:50:27 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -5,31 +5,24 @@ const video = document.getElementById("video1");
 	// Update the progress bar as the video plays
 	video.addEventListener ("timeupdate",function(){
 		// highlight text as video plays
+
 		for (let i = 0;i < lines.length; i++) {
       let current = video.getCurrentTime();
       let start =lines[i].getAttribute("data-start");
       let end= lines[i].getAttribute("data-end");
-		  if (current > start && current < end){
+			console.log(current +""+ start + ""+ end);
+		  if (current > start & current < end){
 		    lines[i].style.color = "#e5a01e";
 		  } else {
 		    lines[i].style.color = "";
       }
 		}
+		// video playes at slected senteance
+		for (let i = 0; i < lines.length; i += 1){
+				 lines[i].addEventListener('click', (click) => {
+					 let timeClick = click.target.getAttribute("data-start");
+					 video.setCurrentTime(timeClick);
+				 });
+				 }
   })
   });
-
-
-
-
-// // var video = document.getElementsByTagName("video")
-// var width = window.matchMedia("(max-width:768px)")
-//
-// function myFunction(width){
-//   if (x.matches){
-//     document.getElementsByTagName("video").setAttribute("width", "480");
-//   }
-// }
-//
-//
-// myFunction(width) // Call listener function at run time
-// width.addListener(myFunction) // Attach listener function on state changes
